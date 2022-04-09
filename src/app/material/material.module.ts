7217bfe0f5b7703ba3e16c3606d0ad02a9317ad9
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-09 18:44:12 +0530

    Button Toggle attributes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button'
 
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 const MaterialComponent=[
   MatButtonModule,
-  MatButtonModule
+  MatButtonModule,
+  MatButtonToggleModule
 ]
 
 @NgModule({
