0462467b73b45ce1fe88c279b0be341d0ea19b3c
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-09 17:34:16 +0530

    button component in material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypographyComponent
+    TypographyComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
