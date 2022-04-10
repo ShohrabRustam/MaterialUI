4aefd9dbeb45ff0c6854ad97c5ea33da2ddc3ce3
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-10 14:16:16 +0530

    add MatSidenavModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,7 +8,7 @@ import {MatIconModule} from '@angular/material/icon'
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 const MaterialComponent=[
@@ -18,7 +18,8 @@ const MaterialComponent=[
   MatIconModule,
   MatBadgeModule,
   MatProgressSpinnerModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatSidenavModule
 ]
 
 @NgModule({
