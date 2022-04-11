8774a7ec8e87daa93622537cd53b903315b716cc
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-11 23:40:51 +0530

    snakeBar on click and on actio the snakebar triggered

--- src/app/sanckbar/sanckbar.component.ts
+++ src/app/sanckbar/sanckbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-sanckbar',
@@ -8,12 +8,18 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 })
 export class SanckbarComponent implements OnInit {
 
-  constructor(private snackBar: MatSnackBar ) { }
+  constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
-  openSnakeBar(message:any,action:any){
-    this.snackBar.open(message,action);
+  openSnakeBar(message: any, action: any) {
+    let snackBarRef = this.snackBar.open(message, action);
+    snackBarRef.afterDismissed().subscribe(() => {
+      console.log("The sanckBar was dismissed ");
+    });
+    snackBarRef.onAction().subscribe(()=>{
+      console.log("The SankeBar Was triggered !");
+    })
   }
 
 }
