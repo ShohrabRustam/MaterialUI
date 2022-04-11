89ffcd04bb45484e00c167d6d66ef1bdf801dc18
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-11 11:28:29 +0530

    option in the autocomplete

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutocompleteComponent implements OnInit {
 
+  option:string[]=["Angular", "React", "Vue"];
   constructor() { }
 
   ngOnInit(): void {
